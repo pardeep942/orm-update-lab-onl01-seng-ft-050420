61588ce3ab507ad452268bed1eb89df7ef78ce90
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 18:19:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,7 +36,7 @@ attr_reader :id
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][1]
     end
   
 end
