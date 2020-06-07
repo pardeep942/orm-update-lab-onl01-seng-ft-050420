8f0d58ec4ac7a75392d75b4de97c10b4cfaabaaa
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 18:24:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ attr_reader :id
     end
     
     def self.create(name:, grade:)
-    student = Students.new(name, grade)
+    student = Student.new(name, grade)
     student.save
     student
   end
