6dbe4049f1ac0d72ad10df52f2d598cc62ac5d0c
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 18:35:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,13 +31,18 @@ attr_reader :id
     
     
     def save
-    sql = <<-SQL 
-        INSERT INTO students (name, grade)  VALUES (?,?)
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES (?, ?)
       SQL
 
       DB[:conn].execute(sql, self.name, self.grade)
+
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
+    
     
     def self.create(name:, grade:)
    student_new = self.new(name, grade)
