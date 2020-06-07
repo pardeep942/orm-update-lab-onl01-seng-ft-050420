6342349d11f63f4841cdaed3fc482b51b442e644
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 18:13:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,17 @@ attr_reader :id
     def self.drop_table
       DB[:conn].execute("DROP TABLE IF EXISTS students")
     end
+    
+    def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
+ 
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    
+  end
       
   
 end
