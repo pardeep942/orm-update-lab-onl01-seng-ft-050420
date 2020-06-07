5df56090cac486f405ecb6665ac1cc6bbe0d75ba
PARDEEP KAUR <pkaur84@aol.com>
2020-06-07 18:27:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,9 +54,7 @@ attr_reader :id
   
   def self.find_by_name(name)
     sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
-  end  
-  
+    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+  end
 end
 
